6cf67c5d97b55ae066710d72f073a56912e2c89c
poramo <poramo@gmail.com>
2011-08-29 09:05:36 -0300

    Se creo @matrix en la clase CSV.

--- 1-perceptron-simple-multicapa/lib/perceptronSimple/csv.rb
+++ 1-perceptron-simple-multicapa/lib/perceptronSimple/csv.rb
@@ -1,20 +1,21 @@
 module PerceptorSimple
   class Csv
-    attr_accessor :file_path
+    attr_accessor :file_path, :matrix
 
     def initialize(file_path)
       @file_path = file_path
+      @matrix = to_a
     end
 
     def to_a
       array = IO.readlines(@file_path)
-      matriz = to_integers(array)
+      @matrix = to_integers(array)
     end
 
     def to_integers(array)
-      matrix = Array.new
-      array.each { |a|  matrix << a.split(',') }
-      matrix.each { |s| s.collect! { |x| x.to_i } }
+      matrizAuxiliar = Array.new
+      array.each { |a|  matrizAuxiliar << a.split(',') }
+      matrizAuxiliar.each { |s| s.collect! { |x| x.to_i } }
     end
   end
 end
