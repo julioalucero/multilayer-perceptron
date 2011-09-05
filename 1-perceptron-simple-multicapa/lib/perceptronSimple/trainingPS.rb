7b031c1a99bded3d41324683bc8deed1324654f4
poramo <poramo@gmail.com>
2011-09-05 08:12:53 -0300

    Corrección general, el método funciona.

--- 1-perceptron-simple-multicapa/lib/perceptronSimple/trainingPS.rb
+++ 1-perceptron-simple-multicapa/lib/perceptronSimple/trainingPS.rb
@@ -1,32 +1,42 @@
 module PerceptorSimple
   class TrainingPS
-    attr_accessor :nInputs, :w, :umbral, :w0
+    attr_accessor :nInputs, :w, :nu, :umbral, :epocas
 
-    def initialize(nInputs, umbral)
+    def initialize(nInputs, nu, epocas)
       @nInputs = nInputs
+      @nu = nu
+      @umbral = 2 * 0.5 * rand - 0.5
+      @epocas = epocas
       @w = initializeRandom
-      @umbral = umbral
-      @w0 = 2 * 0.5 * rand - 0.5
     end
 
     def initializeRandom
-      # TODO debería saber solo cuantas entradas tienen
-      [2 * 0.5 * rand - 0.5, 2 * 0.5 * rand - 0.5]
+      @w=Array.new
+      @nInputs.times do
+         @w.push(2 * 0.5 * rand - 0.5)
+      end
+      p @w
     end
 
     # recibe el array de entrenamiento.
     # entrena y actualiza los pesos
     def training(matrix)
-      matrix.each do |training|
-        y = calculate(training)
-        update(training, y)
+      @epocas.times do
+        matrix.each do |training|
+          y = calculate(training)
+          update(training, y)
+        end
       end
     end
 
     def calculate(training)
       y = dot_product(training)
-      y = y - w0
-      y < 0 ? -1 : 1
+      y = y - @umbral
+      y = sigmoide(y,1)
+    end
+
+    def sigmoide(y,a)
+      y= (1 -Math.exp(-a*y)) / (1 + Math.exp(-a*y))
     end
 
     # w(n+1) = w(n) - umbral*x(n)
@@ -42,21 +52,21 @@ module PerceptorSimple
     end
 
     # umbral/2 [yd - y(n)] * x(n)
-    def constantProduct(training, yd)
+    def constantProduct(training, y)
       shift = []
-      constant = @umbral/2 * (yd - training.last)
-      for i in 0..1
+      constant = @nu/2 * (training.last - y)
+      for i in 0..(@nInputs - 1)
         shift << training[i] * constant
       end
-      @w0 = @w0 + @umbral * (yd - training.last) * -1
+      @umbral = @umbral - constant
       shift
     end
 
     # w(n) - constantProduct
     def subtractArray(shift)
       wUpdate = []
-      for i in 0..1
-        wUpdate << @w[i] - shift[i]
+      for i in 0..(@nInputs -1)
+        wUpdate << @w[i] + shift[i]
       end
       wUpdate
     end
