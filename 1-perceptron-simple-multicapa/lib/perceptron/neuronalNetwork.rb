6909db3e4f66dd34c20d5bdc8c3f7420a98ec5bd
poramo <poramo@gmail.com>
2011-09-07 10:51:09 -0300

    initializeIndex devuelve el array correctoa @indiceEntradas.

--- 1-perceptron-simple-multicapa/lib/perceptron/neuronalNetwork.rb
+++ 1-perceptron-simple-multicapa/lib/perceptron/neuronalNetwork.rb
@@ -1,14 +1,14 @@
 module Perceptor
   class NeuronalNetwork
-    attr_accessor  :capas, :numNeuronas, :salidas, :entradas, :vectorCapas, :indiceEntradas
+    attr_accessor  :capas, :entradas, :numNeuronas, :salidas, :vectorCapas, :indiceEntradas
 
-     # @capas = capas numero de capas.
-     # @cantIter = Punto de corte, cantidad de entrenamientos.
+     # @capas = capas numero de capas ocultas (contando la de entrada)
+     # @entradas = Contiene las entradas principales, vector.Ej. x = [x1, x2, x3, y]
      # @numNeuron = Contiene vector con la cant de neuronas x capa. Ej [3,2].
-     # @entradas = Contiene las entradas principales, vector.
+     # @cantIter = Punto de corte, cantidad de entrenamientos.
      # @indiceEntradas = Cantidad de entradas en cada capa. Ej. [2,1].
      # @vectorCapas = Contiene las neuronas de cada capa.# Ej. [[neuron1, neuron2],[neuron3]]
-     def initialize(capas,entradas,numNeuronas,iteraciones)
+     def initialize(capas, entradas, numNeuronas, iteraciones)
        @capas = capas
        @entradas = entradas
        @numNeuron = numNeuronas
@@ -17,9 +17,14 @@ module Perceptor
        @vectorCapas = initializeRed()
      end
 
+     # recorre cada capa y guarda la cantidad de entradas que posee.
      def initializeIndex
-       #TODO
-       3
+       indiceEntradas = Array.new
+       indiceEntradas << @entradas.length - 1 #La ultima entrada es el valor esperado
+       for i in 1..@capas do #identificar cual es la capa 0 o la 1
+         indiceEntradas << @numNeuron[i-1]
+       end
+       indiceEntradas
      end
 
      def initializeRed
