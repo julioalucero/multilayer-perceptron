f89bc9120af7d5c36474cee83e4bed7f279f2cb6
poramo <poramo@gmail.com>
2011-09-19 20:57:21 -0300

    Calcula los centroides para x e  y.

--- 2-RBF-SOM/lib/rbfSom/rbf.rb
+++ 2-RBF-SOM/lib/rbfSom/rbf.rb
@@ -1,17 +1,58 @@
 module RbfSom
   class Rbf
-    attr_accessor :k, :conjuntos, :patrones
+    attr_accessor :k, :patrones, :centroides
 
     def initialize(k, patrones)
       @k = k
-      @conjuntos = []
+      @patrones = []
+      @centroides = []
       inicializarConjuntos(patrones)
+      calcularCentroides
     end
 
     def inicializarConjuntos(patrones)
       patrones.each do | patron |
-        @conjuntos << { :patron => patron, :clase => rand(@k) }
+        @patrones << { :patron => patron, :clase => rand(@k) }
       end
     end
+
+    def calcularCentroides
+      conjunto = []
+      @k.times do |i|
+        conjunto = getConjunto(i)
+        @centroides << media(conjunto)
+      end
+    end
+
+    def getConjunto(k)
+      conjunto = []
+      @patrones.each do | patron |
+        conjunto << patron[:patron] if patron[:clase] == k
+      end
+      conjunto
+    end
+#    def entrenar
+#      medias = calcularMedias
+#    end
+#
+    # Luego va a tener q ser más generico, para n dimensiones
+    def media(conjunto)
+      x = 0.0
+      y = 0.0
+      conjunto.each do  |patron|
+        x += patron.first
+        y += patron[1]
+      end
+      [x/conjunto.length, y/conjunto.length]
+    end
+
+    def normaEuc(patron, centroide)
+      suma=0
+      for i in 0...patron.length
+        suma += patron[i]-centroide[i]
+      end
+      suma**2
+    end
+
   end
 end
