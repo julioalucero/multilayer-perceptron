e827e338e5cb8069159e839289f5878ff0a205e0
poramo <poramo@gmail.com>
2011-09-19 19:56:52 -0300

    Inicializa y guarda los patrones.

--- 2-RBF-SOM/lib/rbfSom/rbf.rb
+++ 2-RBF-SOM/lib/rbfSom/rbf.rb
@@ -1,12 +1,17 @@
 module RbfSom
   class Rbf
-    attr_accessor :k, :patrones, :conjuntos
+    attr_accessor :k, :conjuntos, :patrones
 
     def initialize(k, patrones)
       @k = k
-      @patrones = patrones
       @conjuntos = []
+      inicializarConjuntos(patrones)
     end
 
+    def inicializarConjuntos(patrones)
+      patrones.each do | patron |
+        @conjuntos << { :patron => patron, :clase => rand(@k) }
+      end
+    end
   end
 end
