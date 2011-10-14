9cad8e8a8e0f7f8aaa4aa8446d9852422f70fbac
Julio Lucero <poramo@gmail.com>
2011-10-14 09:05:06 -0300

    fuzzification

--- 3-sistemas-borrosos/lib/fam/fuzzy.rb
+++ 3-sistemas-borrosos/lib/fam/fuzzy.rb
@@ -1,6 +1,6 @@
 module Fam
   class Fuzzy
-    attr_accessor :tiempo, :puertaAbierta, :tempRef 
+    attr_accessor :tiempo, :puertaAbierta, :tempRef
     attr_accessor :tempExt, :tempInt,:voltaje, :amperes
     attr_accessor :conjuntoEntrada
 
@@ -16,25 +16,25 @@ module Fam
       @tiempo = tiempo
       @puertaAbierta = false
       @tempRef = crear_temperatura_referencia
-      @tempInt = [] 
-      #@tempExt 
-      @conjuntoEntrada = [[1.0, 0.25, 0.0, 0.0, 0.0, 0.0], [0.25, 1.0, 0.25, 0.0, 0.0, 0.0] , [0, 0.1, 1, 0.1, 0.0, 0.0], [0.0, 0.0, 0.25, 1.0, 0.25, 0.0], [0.0, 0.0, 0.0, 0.25, 1.0, 0.25] ]
-
+      @tempInt = []
+      # @conjuntoEntrada = [[1.0, 0.25, 0.0, 0.0, 0.0, 0.0], [0.25, 1.0, 0.25, 0.0, 0.0, 0.0] , [0, 0.1, 1, 0.1, 0.0, 0.0], [0.0, 0.0, 0.25, 1.0, 0.25, 0.0], [0.0, 0.0, 0.0, 0.25, 1.0, 0.25] ]
+      @conjuntoEntrada = [[0.0, 15.0, 10.0],[10.0, 20.0, 15.0], [18.0, 22.0, 20.0], [20.0, 30.0, 25.0], [25.0, 40.0, 30.0]]
     end
 
     def resolver
-      @tiempo.times do
-       if @tiempo % 3600 == 0 then @puertaAbierta == true
-       end
-       fuzzyfication
-       codification
-       composition
-       defuzzzyfication
-      end
+     # @tiempo.times do |t|
+     #  if t % 3600 == 0 then
+     #    @puertaAbierta == true
+     #  end
+       fuzzyfication(15)
+       # codification
+       # composition
+       # defuzzzyfication
+  #    end
     end
 
-    def temperaturaExterna(tiempo)    #nos dice la temperatura en un instante dado
-      @tempExt = 10.0 + (2.0/@tiempo) *tiempo
+    def temperaturaExterna(tiempo) # nos dice la temperatura en un instante dado
+      @tempExt = 10.0 + (2.0/ @tiempo) * tiempo
     end
 
     def crear_temperatura_referencia
@@ -47,13 +47,29 @@ module Fam
 
     def tempInterior(t)
       if(@puertaAbierta)
-        @tempInt << 0.169*@tempInt[t-1] + 0.831 *@tempExt + 0.112* (@amperes**2.0) - 0.002*@voltaje
+        @tempInt << 0.169 * @tempInt[t-1] + 0.831 * @tempExt + 0.112 * (@amperes**2.0) - 0.002*@voltaje
       else
-       @tempInt << 0.912*@tempInt[t-1] + 0.088 *@tempExt + 0.604* (@amperes**2.0) - 0.0121*@voltaje
+        @tempInt << 0.912 * @tempInt[t-1] + 0.088 * @tempExt + 0.604 * (@amperes**2.0) - 0.0121*@voltaje
       end
     end
 
-    def fuzzyfication
+    def fuzzyfication(t)
+      temperaturaExterna(t)
+      grados = []
+      @conjuntoEntrada.each do |coord|
+        grados << pertenencia(coord[0], coord[1], coord[2], t)
+      end
+      grados
+    end
+
+    def pertenencia(a, b, c, x)
+      if x >= a  &&  x <= c then
+        0.25 + (0.75 / (c-a)) * (x - a)
+      elsif x > c && x <= b then
+        1.0 - (0.75 / (b-c)) * (x-c)
+      else
+        0
+      end
     end
   end
 end
