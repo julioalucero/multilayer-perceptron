3e0067857d4ed44a33261c46ffbf8a3188cf8edb
Julio Lucero <poramo@gmail.com>
2014-08-28 19:49:42 -0300

    Add test for exercise 2.b.

--- spec/perceptron/neuron_spec.rb
+++ spec/perceptron/neuron_spec.rb
@@ -35,6 +35,23 @@ module Perceptron
       matrixPruebas.shuffle!
     end
 
+    def matrix_3(csv, desviation, quantity)
+      matrixPrueba1 = MyRandom::SpecialsRandom.new(desviation, quantity, -1.0, -1.0, -1.0,  1)
+      matrixPrueba2 = MyRandom::SpecialsRandom.new(desviation, quantity, -1.0, -1.0,  1.0,  1)
+      matrixPrueba3 = MyRandom::SpecialsRandom.new(desviation, quantity, -1.0,  1.0, -1.0,  1)
+      matrixPrueba4 = MyRandom::SpecialsRandom.new(desviation, quantity, -1.0,  1.0,  1.0,  1)
+      matrixPrueba5 = MyRandom::SpecialsRandom.new(desviation, quantity,  1.0, -1.0, -1.0, -1)
+      matrixPrueba6 = MyRandom::SpecialsRandom.new(desviation, quantity,  1.0, -1.0,  1.0, -1)
+      matrixPrueba7 = MyRandom::SpecialsRandom.new(desviation, quantity,  1.0,  1.0, -1.0,  1)
+      matrixPrueba8 = MyRandom::SpecialsRandom.new(desviation, quantity,  1.0,  1.0,  1.0, -1)
+
+      matrixPruebas = matrixPrueba1.matrix +  matrixPrueba2.matrix + matrixPrueba3.matrix +
+        matrixPrueba4.matrix +  matrixPrueba5.matrix + matrixPrueba6.matrix +
+        matrixPrueba7.matrix +  matrixPrueba8.matrix + csv.matrix
+
+      matrixPruebas.shuffle!
+    end
+
     describe "or exercise" do
       it "all the value should be between (0.95, 1.09)" do
         csv_or = MyCsv::SpecialsCsv.new('or.csv')
@@ -67,7 +84,7 @@ module Perceptron
       end
     end
 
-    describe "Tabla 2 exercise" do
+    describe "Tabla 2.a exercise" do
       it "all the value should be between (0.95, 1.09)" do
         csv_tabla2 = MyCsv::SpecialsCsv.new('tabla2a.csv')
 
@@ -95,6 +112,35 @@ module Perceptron
         end
 
         (sumaErrores * 100 / 1000).should be_between(18, 35)
+      end
+    end
+
+    describe "Tabla 2.b exercise" do
+      it "all the value should be between (0.95, 1.09)" do
+        csv_tabla2 = MyCsv::SpecialsCsv.new('tabla2b.csv')
+
+        neuron = Neuron.new(2, 0.5, 10, false)
+
+        # asignamos nuestra matrixPruebas al objeto csv_tabla2
+        csv_tabla2.matrix = matrix_3(csv_tabla2, 0.10, 624)
+        porcentaje = 80
+        cantidadParticiones = 50
+        csv_tabla2.createIndices(porcentaje, cantidadParticiones)
+
+        particiones = []
+        for i in 0..49 do
+          neurona = Perceptron::Neuron.new(3, 0.2, 1,true)
+          neurona.trainingWithIndices(csv_tabla2.matrix, csv_tabla2.trainingIndices[i])
+          neurona.test(csv_tabla2.matrix, csv_tabla2.testIndices[i])
+          particiones << neurona
+        end
+
+        sumaErrores = 0
+        particiones.each do |epoca|
+          sumaErrores += epoca.error
+        end
+
+        (sumaErrores * 100 / 1000).should be_between(0, 1)
       end
     end
   end
