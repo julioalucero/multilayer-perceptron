1e203cb32978e4a1e2572e037456ea0a4ef7a949
Julio Lucero <poramo@gmail.com>
2014-08-28 20:18:31 -0300

    Refactoring to reduce methods.

--- lib/perceptron/neuron.rb
+++ lib/perceptron/neuron.rb
@@ -79,35 +79,21 @@ module Perceptron
     end
 
     # w(n+1) = w(n) - umbral * x(n)
+    # shift = (@nu / 2) . [yd - y(n)] . x(n)
     def update(training, y)
-      shift = constantProduct(training, y)
-      @w = subtractArray(shift)
+      constant = (@nu / 2) * (training.last - y)
+
+      training.each_with_index do |input, i|
+        next if input == training.last
+        @w[i] += input * constant
+      end
+      @umbral = @umbral - constant
     end
 
     def dot_product(training)
       sum = 0
       @w.zip(training) { |a, b| sum += a * b }
       sum
-    end
-
-    # (@nu / 2) . [yd - y(n)] . x(n)
-    def constantProduct(training, y)
-      shift = []
-      constant = (@nu / 2) * (training.last - y)
-      for i in 0..(@nInputs - 1)
-        shift << training[i] * constant
-      end
-      @umbral = @umbral - constant
-      shift
-    end
-
-    # w(n) - constantProduct
-    def subtractArray(shift)
-      wUpdate = []
-      for i in 0..(@nInputs -1)
-        wUpdate << @w[i] + shift[i]
-      end
-      wUpdate
     end
 
     def graficar
